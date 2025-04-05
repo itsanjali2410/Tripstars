2bae7101b2ab87a70df745372e7e6899cebf8cb9
Anjali <itsanjali2410@gmail.com>
2025-04-05 12:45:00 +0530

    testimonial

--- src/pages/Home/sections/VideoTestimonials.tsx
+++ src/pages/Home/sections/VideoTestimonials.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import bgImage from "../../../assets/images/backgrounds/Videotestimonials-bg.webp";
 // import Video8 from "../../../assets/Videos/Testimonial gif/8.gif";
-import Video1 from "../../../assets/Videos/Testimonial gif/Testimonial AVIF/Testimonial(1).avif";
+import Video1 from "../../../assets/Videos/Testimonial gif/1.gif";
 import Video2 from "../../../assets/Videos/Testimonial gif/2.gif";
 import Video3 from "../../../assets/Videos/Testimonial gif/3.gif";
 import Video4 from "../../../assets/Videos/Testimonial gif/4.gif";
