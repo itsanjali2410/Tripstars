bc2f824371e0d7408f0ff8fc7d038ce127ff1c32
itsanjali2410 <itsanjali2410@gmail.com>
2025-05-09 14:37:46 +0530

    thankyou page

--- src/components/common/thankyou.tsx
+++ src/components/common/thankyou.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 import TrustindexWidget from "../../pages/Home/sections/TrustindexWidget";
+import VideoTestimonials from "../../pages/Home/sections/VideoTestimonials";
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: black;
+`;
+
 const ThankYouContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  background-image: url(""); /* Example travel image */
+  padding: 80px 20px;
+  background-image: url(""); /* Add a travel image URL here if needed */
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   text-align: center;
-  padding: 20px;
   color: white;
 `;
-
 
 const Title = styled.h1`
   font-size: 36px;
@@ -24,14 +31,14 @@ const Title = styled.h1`
 `;
 
 const Message = styled.p`
-  font-size: 18px;                                                                                                       
-  color: #555;
+  font-size: 18px;
+  color: #ccc;
   margin-bottom: 20px;
 `;
 
 const HomeButton = styled.a`
   padding: 10px 20px;
-  background:rgb(9, 9, 17);
+  background: rgb(9, 9, 17);
   color: white;
   text-decoration: none;
   font-size: 16px;
@@ -43,15 +50,29 @@ const HomeButton = styled.a`
   }
 `;
 
+const SectionWrapper = styled.div`
+  padding: 40px 20px;
+  background-color: #111;
+  color: white;
+`;
+
 const ThankYou: React.FC = () => {
   return (
-    
-    <ThankYouContainer>
-      <Title>🎉 Thank You!</Title>
-      <Message>Email sent successfully to Admin!</Message>
-      <HomeButton href="/">Go Back to Home</HomeButton>
-      
-    </ThankYouContainer>
+    <PageWrapper>
+      <ThankYouContainer>
+        <Title>🎉 Thank You!</Title>
+        <Message>Email sent successfully to Admin!</Message>
+        <HomeButton href="/">Go Back to Home</HomeButton>
+      </ThankYouContainer>
+
+      <SectionWrapper>
+        <TrustindexWidget />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <VideoTestimonials />
+      </SectionWrapper>
+    </PageWrapper>
   );
 };
 
